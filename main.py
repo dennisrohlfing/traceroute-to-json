a74e32ae5b335f9d8560e6648b4d3734684af8c1
Dennis Rohlfing <rohlfing93@googlemail.com>
2020-06-16 21:13:57 +0200

    adding RIPE ipwhois request

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 import json
-import whois
+from ipwhois import IPWhois
 import time
-
 
 fileName = "./data2.json"
 
@@ -132,11 +131,49 @@ def saveJSONtoDisk():
     return 0
 
 
+def fetchInfo(ip):
+    try:
+        obj = IPWhois(ip)
+        result = obj.lookup_whois()
+        description = {"labels": [], "range": ""}
+        for net in result["nets"]:
+            description["labels"].append(net["description"])
+
+        try:
+            description["range"] = result["nets"][0]["range"]
+        except:
+            description["range"] = "not Available"
+        return description
+    except:
+        return {
+            "labels": [
+            ],
+            "range": ""
+        }
+
+
+def addRipeNCCdescribtion():
+    if not os.path.isfile(fileName):
+        print('File does not exist.')
+    else:
+        # Open the file as f.
+        # The function readlines() reads the file.
+        fileRead = open(fileName, 'r')
+        data = json.loads(fileRead.read())
+
+        fileRead.close()
+
+        fileSave = open(fileName, 'w')
+        fileSave.write(json.dumps({int(x): data[x] for x in data.keys()}, indent=4, sort_keys=True))
+        fileSave.close()
+
+
 def main():
+    print(fetchInfo("134.102.22.124"))
     # writeToJSON(1, 1, 1, 1)
-    for x in range(0, 10):
-        parseToJSON(executeTraceroute())
-        time.sleep(5)
+    # for x in range(0, 10):
+    #     parseToJSON(executeTraceroute())
+    #     time.sleep(2)
 
 
 if __name__ == "__main__":
